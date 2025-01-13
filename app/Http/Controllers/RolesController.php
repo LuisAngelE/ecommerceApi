196df4f2924db64afa00edf53b2f40c2a5208ab5
luisangeldp <luisangelem.dp@gmail.com>
2025-01-13 11:17:00 -0600

    controllers

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Roles;
 use Illuminate\Http\Request;
 
 class RolesController extends Controller
 {
-    //
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Roles $coupons)
+    {
+        //
+    }
+
+    public function update(Request $request, Roles $coupons)
+    {
+        //
+    }
+
+    public function destroy(Roles $coupons)
+    {
+        //
+    }
 }
